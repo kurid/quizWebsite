9d0dfca4a478465b66a09250e9a1afdd90c360a2
qeti uridia <kurid11@freeuni.edu.ge>
2013-05-28 21:05:38 +0400

    accountManager

--- quizWebsite/src/web/AccountManager.java
+++ quizWebsite/src/web/AccountManager.java
@@ -7,16 +7,38 @@ public class AccountManager implements Manager{
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
-	public boolean isCorrect(String nickName, String Password) {
-		// TODO Auto-generated method stub
+	private boolean nickNameExist(String nickName){
+		//TODO:
 		return false;
 	}
+	
+	private boolean passwordIsCorrect(String nickName, String password){
+		//TODO:
+		return false;
+	}
+	
+	@Override
+	public boolean isCorrect(String nickName, String password) {
+		return nickNameExist(nickName) && passwordIsCorrect(nickName, password);
+	}
+	
+	private void addInDataBase(String name, String surname, String nickName,
+			String Password, String mail) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private static final int successful_add = 1;
+	private static final int already_exist = 2;
+	private static final int mail_incorrect = 3; 
 
 	@Override
 	public int addAccount(String name, String surname, String nickName,
 			String Password, String mail) {
-		// TODO Auto-generated method stub
-		return 0;
+		if(!mail.contains("@")) return mail_incorrect;
+		if(nickNameExist(nickName)) return already_exist;
+		addInDataBase(name, surname, nickName, Password, mail);
+		return successful_add;
 	}
+
 }
--- quizWebsite/src/web/MyDB.java
+++ quizWebsite/src/web/MyDB.java
@@ -97,7 +97,6 @@ public class MyDB {
 	}
 	
 	
-	
 	public static void close() {
 		try {
 			con.close();
