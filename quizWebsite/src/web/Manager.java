a8da144c7efef8369d2512a89ab48ae972885a67
qeti uridia <kurid11@freeuni.edu.ge>
2013-05-28 11:46:41 +0400

    manager interface. wakitxet metodebis komentarebi

--- quizWebsite/src/web/Manager.java
+++ quizWebsite/src/web/Manager.java
@@ -2,7 +2,12 @@ package web;
 
 public interface Manager {
 
+	
+	// amowmebs sworia tu ara sheyvanili nickname da password
 	public boolean isCoreckt(String nickName, String Password);
 	
+	/*
+	 * ambrunebs int-s imis shesabamisad ra shedegit dasrulda account-is sheqmis mcdeloboba
+	 */
 	public int addAccount(String name, String surname, String nickName, String Password, String mail);
 }
