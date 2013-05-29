2fd200ec49448136f501b4492823327574f640d0
sabagogolidze <sggol11@freeuni.edu.ge>
2013-05-29 13:11:59 +0400

    my dbs testebi daweret aq

---
+++ quizWebsite/src/web/MyDbTest.java
@@ -0,0 +1,21 @@
+package web;
+
+import junit.framework.TestCase;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class MyDbTest extends TestCase {
+
+	public void testGetFriends() {
+		// qveda shemowmeba shecdomas agdebs rato arvici .
+		// mydb.getfriends sworad abrunebs lists assertis bralia shecdoma
+		// assertEquals(Arrays.asList(new int[] { 2, 3 }),MyDB.getFriends(1) );
+		assertEquals("saba9993", MyDB.getNickName(4));
+		assertEquals("saba", MyDB.getName(4));
+		assertEquals("gogolidze", MyDB.getSurname(4));
+		MyDB.sendMassage(1, 2, "a");
+		MyDB.sendChallenge(1, 2, 1);
+	}
+}
