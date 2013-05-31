4f4e07ea8ee3568620cb67a0e56f5036a506e04c
qeti uridia <kurid11@freeuni.edu.ge>
2013-05-31 22:30:21 +0400

    password-ის დაჰეშირება

--- quizWebsite/src/web/AccountManager.java
+++ quizWebsite/src/web/AccountManager.java
@@ -1,7 +1,13 @@
 package web;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class AccountManager implements Manager{
+	
 	private int id;
+	private static MessageDigest messageDigest;
+	
 	
 	public AccountManager() {
 		// TODO Auto-generated constructor stub
@@ -12,22 +18,58 @@ public class AccountManager implements Manager{
 	}
 	
 	private boolean passwordIsCorrect(String nickName, String password){
+		try {
+			messageDigest = MessageDigest.getInstance("SHA");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		messageDigest.update(password.getBytes());
+		String passwordHash = hexToString(messageDigest.digest());
 		String realPass = MyDB.getPassword(MyDB.getId(nickName));
-		return realPass.equals(password);
+		System.out.println("hash code" + passwordHash);
+		return realPass.equals(passwordHash);
 	}
+	
+	
 	
 	@Override
 	public boolean isCorrect(String nickName, String password) {
 		return nickNameExist(nickName) && passwordIsCorrect(nickName, password);
 	}
 	
+	
+	
 	private int addInDataBase(String name, String surName, String nickName,
-			String password, String mail) {
-		
-		
-		 MyDB.addAccount(name, surName, nickName, password, mail);
-		 return MyDB.getId(nickName);
+		String password, String mail) {
+		try {
+			messageDigest = MessageDigest.getInstance("SHA");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		messageDigest.update(password.getBytes());
+		String passwordHash = hexToString(messageDigest.digest());
+		System.out.println("hash code" + passwordHash);
+		MyDB.addAccount(name, surName, nickName, passwordHash, mail);
+		return MyDB.getId(nickName);
 	}
+	
+	
+	/*
+	 * Given a byte[] array, produces a hex String, such as "234a6f". with 2
+	 * chars for each byte in the array. (provided code)
+	 */
+	public static String hexToString(byte[] bytes) {
+		StringBuffer buff = new StringBuffer();
+		for (int i = 0; i < bytes.length; i++) {
+			int val = bytes[i];
+			val = val & 0xff; // remove higher bits, sign
+			if (val < 16)
+				buff.append('0'); // leading 0
+			buff.append(Integer.toString(val, 16));
+		}
+		return buff.toString();
+	}
+
 	
 	private boolean mailExists(String mail) {		
 		return MyDB.mailExist(mail);
