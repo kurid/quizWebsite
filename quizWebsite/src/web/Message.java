30556f94171fab77aad4a01b28fe7b6777a51d97
mate <mate@mate-PC>
2013-06-09 16:50:19 -0700

    isUnread davamate Message klass

--- quizWebsite/src/web/Message.java
+++ quizWebsite/src/web/Message.java
@@ -5,15 +5,21 @@ import java.util.Date;
 public class Message extends Notification{
 	
 	String text;
+	boolean isUnread;
 	
-	public Message(int to, int from, Date date, String text){
+	public Message(int to, int from, Date date, String text, boolean isUnread){
 		senderID = from;
 		recieverID = to;
 		this.date = date;
 		this.text = text;
+		this.isUnread = isUnread;
 	}
 	
 	public String getText(){
 		return text;
+	}
+	
+	public boolean isUnread(){
+		return isUnread;
 	}
 }
