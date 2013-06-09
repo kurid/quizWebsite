a3678a2c293561445c695c140adaac1fbcddd20c
mate <mate@mate-PC>
2013-06-09 14:45:48 -0700

    ჯავა ფაილები დამავიწყდა :დ

---
+++ quizWebsite/src/web/MessageServlet.java
@@ -0,0 +1,46 @@
+package web;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MessageServlet
+ */
+@WebServlet("/MessageServlet")
+public class MessageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MessageServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Account user = (Account)request.getSession(true).getAttribute("account");
+		List<Message> messages = MyDB.getMessages(user.getId());
+		request.getSession(true).setAttribute("messages", messages);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("ShowMessages.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
