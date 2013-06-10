cb268e9bd66fb86e6d0b0c0a5dd483f6986b2bc8
Rakhdakh <giglemiani@gmail.com>
2013-06-10 10:51:08 +0400

    მაპატიე, თუ შეგიძლია aseti commiti, mara ro sawiroa?

--- quizWebsite/src/quiz/TakeQuizServlet.java
+++ quizWebsite/src/quiz/TakeQuizServlet.java
@@ -126,7 +126,7 @@ public class TakeQuizServlet extends HttpServlet {
 			}
 			break;
 		case QuestionFinals.MULTIPLE_CHOICE:
-			answer = request.getParameter("answer");
+			answer = (String) request.getParameter("answer");
 			if (answer != null && ((String)q.getCorrectAnswer().getAnswer()).equals(answer))
 					score=q.getScore();
 			break;
