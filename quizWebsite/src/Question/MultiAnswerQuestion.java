a87b0352b4991f36e90c795613cf5311ddac038a
mate <mate@mate-PC>
2013-06-12 16:33:00 -0700

    checkAnswer-ebi davamate

--- quizWebsite/src/Question/MultiAnswerQuestion.java
+++ quizWebsite/src/Question/MultiAnswerQuestion.java
@@ -1,5 +1,8 @@
 package Question;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import web.MyDB;
 
 public class MultiAnswerQuestion extends Question{
@@ -44,5 +47,40 @@ public class MultiAnswerQuestion extends Question{
 	@Override
 	public String getJspName() {
 		return MULTIPLE_ANSWER_JSP;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public int checkAnswer(RecievedAnswer answer){
+		MMAnswer mma = (MMAnswer)correctAnswer;
+		List<List<String> > correctAnswers = mma.getAnswer();
+		List<String> recieved =  (List<String>) answer.getRecievedAnswer();
+		recieved = filteredList(recieved);
+		int resultScore = 0;
+		for(int i = 0; i < recieved.size(); i++){
+			if(currentAnswerIsCorrect(correctAnswers, recieved.get(i)))
+				resultScore += score;
+		}
+		return resultScore;
+	}
+
+	private List<String>  filteredList(List<String> recieved) {
+		List<String> result = new ArrayList<String>();
+		for(int i = 0; i < recieved.size(); i++){
+			if(!result.contains(recieved.get(i)))
+				result.add(recieved.get(i));
+		}
+		return result;
+	}
+
+	private boolean currentAnswerIsCorrect(List<List<String>> correctAnswers,
+			String currentAnswer) {
+		for(int i = 0; i < correctAnswers.size(); i++){
+			if(correctAnswers.get(i).contains(currentAnswer)){
+				correctAnswers.remove(i);
+				return true;
+			}
+		}
+		return false;
 	}
 }
