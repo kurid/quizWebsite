f383214ffbc2b16b90e67376105a73aac769f4af
mate <mate@mate-PC>
2013-06-11 22:15:02 -0700

    shevqmeni HomePage servleti romelic unda gaushvat tavidan HomePage.jsp-is nacvlad.

---
+++ quizWebsite/src/web/HomePage.java
@@ -0,0 +1,45 @@
+package web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import quiz.QuizHelper;
+
+/**
+ * Servlet implementation class HomePage
+ */
+@WebServlet("/HomePage")
+public class HomePage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomePage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext context = request.getServletContext();
+		QuizHelper.updatePopularQuizes(context);
+		QuizHelper.updateNewQuizes(context);
+		response.sendRedirect("HomePage.jsp");
+	}
+}
