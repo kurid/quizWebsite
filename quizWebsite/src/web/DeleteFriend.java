15e080a3c8d7058431067a1b390bdfc0df453f99
Rakhdakh <giglemiani@gmail.com>
2013-06-13 19:41:23 +0400

    DeleteFriend

---
+++ quizWebsite/src/web/DeleteFriend.java
@@ -0,0 +1,48 @@
+package web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class DeleteFriend
+ */
+@WebServlet("/DeleteFriend")
+public class DeleteFriend extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteFriend() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession Session = request.getSession(true);
+		Account account = (Account) Session.getAttribute("account");
+		Account userAccount = (Account) Session.getAttribute("userAccount");
+		MyDB.deleteFriendship(account.getId(),userAccount.getId());
+		Session.setAttribute("isFriend", false);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("AccountWindow.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
