1f817ab0c321f35c515352c8d247594f1c6c3b84
Rakhdakh <giglemiani@gmail.com>
2013-06-13 07:31:56 +0400

    :(

---
+++ quizWebsite/src/web/AccountServlet.java
@@ -0,0 +1,53 @@
+package web;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ChallengeServlet
+ */
+@WebServlet("/ChallengeServlet")
+public class AccountServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AccountServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Account user = (Account)request.getSession(true).getAttribute("account");
+		int clickedID = MyDB.getId(request.getParameter("nickname"));
+		boolean isMyAccount = false;
+		if (clickedID == user.getId()){
+			isMyAccount=true;
+		} else {
+			Account userAccount = new Account(clickedID);
+			request.getSession().setAttribute("nickname", userAccount);
+		}
+		request.setAttribute("isLookingUp", isMyAccount);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("AccountWindow.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
