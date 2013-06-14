6fff34ea33534f5a87ffd335873cc7110f31254f
qeti uridia <kurid11@freeuni.edu.ge>
2013-06-14 09:50:39 +0400

    /

--- quizWebsite/src/web/Servlest/PromoteAccount.java
+++ quizWebsite/src/web/Servlest/PromoteAccount.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import web.MyDB;
+
 /**
  * Servlet implementation class PromoteAccount
  */
@@ -33,7 +35,8 @@ public class PromoteAccount extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		int accountID = Integer.parseInt(request.getParameter("name"));
+		MyDB.promoteAsAdmin(accountID);
 	}
 
 }
